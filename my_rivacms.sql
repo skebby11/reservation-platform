9e0e1628966b15241b1add2bd55264fbb53ccc5d
Sebastiano Riva <skebby11@gmail.com>
2018-11-18 20:33:30 +0100

    Database file

---
+++ my_rivacms.sql
@@ -0,0 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.7
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Nov 18, 2018 alle 20:32
+-- Versione del server: 5.6.33-log
+-- PHP Version: 5.3.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `my_rivacms`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `prenotazioni`
+--
+
+CREATE TABLE IF NOT EXISTS `prenotazioni` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
+  `cliente` varchar(100) NOT NULL,
+  `arrivo` varchar(50) NOT NULL,
+  `partenza` varchar(50) NOT NULL,
+  `mail` varchar(50) NOT NULL,
+  `phone` varchar(15) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+
+--
+-- Dump dei dati per la tabella `prenotazioni`
+--
+
+INSERT INTO `prenotazioni` (`id`, `cliente`, `arrivo`, `partenza`, `mail`, `phone`) VALUES
+(1, 'Riva Sebastiano', '2018-11-17', '2018-11-18', 'skebby11@gmail.com', '3383576286'),
+(4, 'Nome 2', '2018-11-25', '2018-12-02', 'mail2@gmail.com', '3387481551'),
+(5, 'Nome 3', '2018-12-08', '2018-12-09', 'mail3@gmail.com', '3387481551'),
+(6, 'Nome 4', '2018-12-02', '2018-12-06', 'mail4@gmail.com', '3387481551');
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `user_type` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dump dei dati per la tabella `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `user_type`) VALUES
+(1, 'skebby', '5c8a45f93fc8e01dc82a92828fb6f2a8', 'admin');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
